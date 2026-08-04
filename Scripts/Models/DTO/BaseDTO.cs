e32bbed8866899d408cc7e345d51fb583f1bf9db
chauthanhbinh19 <21521871@gm.uit.edu.vn>
2026-08-04 21:54:30 +0700

    Fix code

---
+++ Scripts/Models/DTO/BaseDTO.cs
@@ -0,0 +1,710 @@
+public class AchievementsDTO
+{
+    public Achievements Achievement { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class AlchemiesDTO
+{
+    public Alchemies Alchemy { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ArchitecturesDTO
+{
+    public Architectures Architecture { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ArtifactsDTO
+{
+    public Artifacts Artifact { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ArtworksDTO
+{
+    public Artworks Artwork { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class AvatarsDTO
+{
+    public Avatars Avatar { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class BadgesDTO
+{
+    public Badges Badge { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class BeveragesDTO
+{
+    public Beverages Beverage { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class BooksDTO
+{
+    public Books Book { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class BordersDTO
+{
+    public Borders Border { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class BuildingsDTO
+{
+    public Buildings Building { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardAdmiralsDTO
+{
+    public CardAdmirals CardAdmiral { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardCaptainsDTO
+{
+    public CardCaptains CardCaptain { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardColonelsDTO
+{
+    public CardColonels CardColonel { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardGeneralsDTO
+{
+    public CardGenerals CardGeneral { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardHeroesDTO
+{
+    public CardHeroes CardHero { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardLivesDTO
+{
+    public CardLives CardLife { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardMilitariesDTO
+{
+    public CardMilitaries CardMilitary { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardMonstersDTO
+{
+    public CardMonsters CardMonster { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardSoldiersDTO
+{
+    public CardSoldiers CardSoldier { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CardSpellsDTO
+{
+    public CardSpells CardSpell { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CollaborationEquipmentsDTO
+{
+    public CollaborationEquipments CollaborationEquipment { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CollaborationsDTO
+{
+    public Collaborations Collaboration { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CoresDTO
+{
+    public Cores Core { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class CurrenciesDTO
+{
+    public Currencies Currency { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class DailyCheckinDTO
+{
+    public DailyCheckin DailyCheckin { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class EmojisDTO
+{
+    public Emojis Emoji { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class EmployeesDTO
+{
+    public Employees Employee { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class EquipmentsDTO
+{
+    public Equipments Equipment { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class EquipmentTypeDTO
+{
+    public EquipmentType EquipmentType { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class FashionsDTO
+{
+    public Fashions Fashion { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class FeaturesDTO
+{
+    public Features Feature { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class FoodsDTO
+{
+    public Foods Food { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ForgesDTO
+{
+    public Forges Forge { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class FurnituresDTO
+{
+    public Furnitures Furniture { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HICASDTO
+{
+    public HICAs HICAs { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HICBsDTO
+{
+    public HICBs HICB { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HIDCsDTO
+{
+    public HIDCs HIDC { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HIENsDTO
+{
+    public HIENs HIEN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HIHNsDTO
+{
+    public HIHNs HIHN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HIINsDTO
+{
+    public HIINs HIIN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HIRNsDTO
+{
+    public HIRNs HIRN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HISNsDTO
+{
+    public HISNs HISN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class HITNsDTO
+{
+    public HITNs HITN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ItemsDTO
+{
+    public Items Item { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class MagicFormationCirclesDTO
+{
+    public MagicFormationCircles MagicFormationCircle { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class MasterBoardDTO
+{
+    public MasterBoard MasterBoard { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class MastersDTO
+{
+    public Masters Master { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class MechaBeastsDTO
+{
+    public MechaBeasts MechaBeast { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class MedalsDTO
+{
+    public Medals Medal { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ModulesDTO
+{
+    public Modules Module { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class OutfitsDTO
+{
+    public Outfits Outfit { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class PatternsDTO
+{
+    public Patterns Pattern { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class PetsDTO
+{
+    public Pets Pet { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class PlantsDTO
+{
+    public Plants Plant { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class PowerManagerDTO
+{
+    public PowerManager PowerManager { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class PuppetsDTO
+{
+    public Puppets Puppet { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class RanksDTO
+{
+    public Ranks Rank { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class RecipeDTO
+{
+    public Recipe Recipe { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class RelicsDTO
+{
+    public Relics Relic { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ResearchsDTO
+{
+    public Researchs Research { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class RobotsDTO
+{
+    public Robots Robot { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class RunesDTO
+{
+    public Runes Rune { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class ScienceFictionsDTO
+{
+    public ScienceFictions ScienceFiction { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class SkillsDTO
+{
+    public Skills Skill { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class SpiritBeastsDTO
+{
+    public SpiritBeasts SpiritBeast { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class SpiritCardsDTO
+{
+    public SpiritCards SpiritCard { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class SSWNsDTO
+{
+    public SSWNs SSWN { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class SymbolsDTO
+{
+    public Symbols Symbol { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class TalismansDTO
+{
+    public Talismans Talisman { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class TeamsDTO
+{
+    public Teams Team { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class TechnologiesDTO
+{
+    public Technologies Technology { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class TitlesDTO
+{
+    public Titles Title { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class TrainsDTO
+{
+    public Trains Train { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class UniversesDTO
+{
+    public Universes Universe { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class UpgradesDTO
+{
+    public Upgrades Upgrade { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class VehiclesDTO
+{
+    public Vehicles Vehicle { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class WeaponsDTO
+{
+    public Weapons Weapon { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
+
+public class WorldsDTO
+{
+    public Worlds World { get; set; }
+    public string Search { get; set; }
+    public string PageSize { get; set; }
+    public string Offset { get; set; }
+    public string UserId { get; set; }
+}
